24dc7220a737c698870e43b2703d421cfbde2f31
page0905 <thtrang14395@gmail.com>
2025-10-26 21:42:08 +0700

    fix

--- server.js
+++ server.js
@@ -4,7 +4,22 @@ const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://page0905.github.io");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 server.use(router);
+
 server.listen(process.env.PORT || 3002, "0.0.0.0", () => {
   console.log("🚀 JSON Server is running");
 });
